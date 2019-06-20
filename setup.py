294e6dc7af1fbb0fd2b473dcf88f1534bb0a99e5
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2019-06-20 11:28:21 -0700

    tryagain

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ config = {
     'packages': [''],
     'package_data': {},
     'setup_requires': [],
-    'dependency_links': ['http://github.com/kundajelab/simdna.git@v0.4.3.1#egg=simdna-0.4.3.1'],
+    'dependency_links': ['git+https://github.com/kundajelab/simdna.git@v0.4.3.1#egg=simdna-0.4.3.1'],
     'install_requires': ['simdna>=0.4.3.1', 'numpy>=1.9', 'matplotlib', 'scipy'],
     'scripts': ['scripts/motif_density_and_position_sim.py'],
     'name': 'revcompexp'
